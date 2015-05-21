f9978160aef1108d439c7d04a05deace9c896fc7
Tyler Akins <fidian@rumkin.com>
2015-05-21 13:42:51 -0500

    Perform DNS resolution at the time of connect
    This also splits the code out into several smaller functions

--- portforward.c
+++ portforward.c
@@ -1,3 +1,7 @@
+/* Licensed under MIT
+ * Original source: https://github.com/antialize/utils/blob/master/portforward.c
+ * It was later licensed:  https://github.com/antialize/utils/issues/1
+ */
 #include <stdio.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
@@ -43,42 +47,12 @@ void com(int src, int dst) {
     exit(0);
 }
 
-int main(int argc, char **argv) {
-    int server_socket, client_socket, forward_socket;
-    struct sockaddr_in server_address, forward_address;
+int open_forwarding_socket(char *forward_name, int forward_port) {
+    int forward_socket;
     struct hostent *forward;
-    int server_port, forward_port;
-    pid_t up, down;
+    struct sockaddr_in forward_address;
 
-    signal(SIGCHLD,  SIG_IGN);
-
-    if (argc < 4) {
-        fprintf(stderr,"Not enough arguments\n");
-        exit(1);
-    }
-
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (server_socket == -1) {
-        DIE("socket");
-    }
-
-    bzero((char *) &server_address, sizeof(server_address));
-    server_port = atoi(argv[1]);
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(server_port);
-
-    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) == -1) {
-        DIE("bind");
-    }
-
-    if (listen(server_socket,40) == -1) {
-        DIE("listen");
-    }
-
-    forward_port = atoi(argv[3]);
-    forward = gethostbyname(argv[2]);
+    forward = gethostbyname(forward_name);
 
     if (forward == NULL) {
         DIE("gethostbyname");
@@ -89,46 +63,129 @@ int main(int argc, char **argv) {
     bcopy((char *)forward->h_addr, (char *) &forward_address.sin_addr.s_addr, forward->h_length);
     forward_address.sin_port = htons(forward_port);
 
-    while (1) {
-        client_socket = accept(server_socket, NULL, NULL);
+    forward_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-        if (client_socket == -1) {
-            DIE("accept");
-        }
+    if (forward_socket == -1) {
+        DIE("socket");
+    }
 
-        up = fork();
+    if (connect(forward_socket, (struct sockaddr *) &forward_address, sizeof(forward_address)) == -1) {
+        DIE("connect");
+    }
 
-        if (up == -1) {
-            DIE("fork");
-        }
+    return forward_socket;
+}
 
-        if (up == 0) {
-            forward_socket = socket(AF_INET, SOCK_STREAM, 0);
+void connect_to_forward(int client_socket, char *forward_name, int forward_port) {
+    int forward_socket;
+    pid_t down_pid;
 
-            if (forward_socket == -1) {
-                DIE("socket");
-            }
+    forward_socket = open_forwarding_socket(forward_name, forward_port);
 
-            if (connect(forward_socket, (struct sockaddr *) &forward_address, sizeof(forward_address)) == -1) {
-                DIE("connect");
-            }
+    // Fork - child forwards traffic back to client, parent sends from client
+    // to forwarded port
+    down_pid = fork();
 
-            down = fork();
+    if (down_pid == -1) {
+        DIE("fork");
+    }
 
-            if (down == -1) {
-                DIE("fork");
-            }
+    if (down_pid == 0) {
+        com(forward_socket, client_socket);
+    } else {
+        com(client_socket, forward_socket);
+    }
+}
 
-            if (down == 0) {
-                com(forward_socket, client_socket);
-            } else {
-                com(client_socket, forward_socket);
-            }
+int open_listening_port(int server_port) {
+    struct sockaddr_in server_address;
+    int server_socket;
 
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_socket == -1) {
+        DIE("socket");
+    }
+
+    bzero((char *) &server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(server_port);
+
+    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) == -1) {
+        DIE("bind");
+    }
+
+    if (listen(server_socket, 40) == -1) {
+        DIE("listen");
+    }
+
+    return server_socket;
+}
+
+void accept_connection(int server_socket, char *forward_name, int forward_port) {
+    int client_socket;
+    pid_t up_pid;
+
+    client_socket = accept(server_socket, NULL, NULL);
+
+    if (client_socket == -1) {
+        DIE("accept");
+    }
+
+    // Fork - Child handles this connection, parent listens for another
+    up_pid = fork();
+
+    if (up_pid == -1) {
+        DIE("fork");
+    }
+
+    if (up_pid == 0) {
+        connect_to_forward(client_socket, forward_name, forward_port);
+        exit(1);
+    }
+
+    close(client_socket);
+}
+
+void parse_arguments(int argc, char **argv, int *server_port, char **forward_name, int *forward_port) {
+    if (argc < 3) {
+        fprintf(stderr, "Not enough arguments\n");
+        fprintf(stderr, "Syntax:  %s listen_port forward_host [forward_port]\n", argv[0]);
+        exit(1);
+    }
+
+    *server_port = atoi(argv[1]);
+
+    if (*server_port < 1) {
+        fprintf(stderr, "Listen port is invalid\n");
+        exit(1);
+    }
+
+    *forward_name = argv[2];
+    
+    if (argc == 3) {
+        *forward_port = *server_port;
+    } else {
+        *forward_port = atoi(argv[3]);
+
+        if (*forward_port < 1) {
+            fprintf(stderr, "Forwarding port is invalid\n");
             exit(1);
         }
+    }
+}
 
-        close(client_socket);
+int main(int argc, char **argv) {
+    int server_port, forward_port, server_socket;
+    char *forward_name;
+
+    parse_arguments(argc, argv, &server_port, &forward_name, &forward_port);
+    signal(SIGCHLD,  SIG_IGN);
+    server_socket = open_listening_port(server_port);
+
+    while (1) {
+        accept_connection(server_socket, forward_name, forward_port);
     }
 
     return 0; 
